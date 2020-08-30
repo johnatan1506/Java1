115ac5b2c1e3e83ea0639030faff9577825a01c1
Ivan <ivan.horivchuk@gmail.com>
2020-08-30 21:41:53 +0200

    Homework 1

---
+++ src/homework3/task1.java
@@ -0,0 +1,52 @@
+package homework3;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class task1 {
+    public static final int BLOCK_WORDS= 15;
+    public static final String[] WORDS = new String[] {
+            "apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+            "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+            "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+    public static void main(String[] args) {
+        System.out.println("Угадай слово из перечисленных" + Arrays.toString(WORDS));
+        Random random = new Random();
+        int word = random.nextInt(WORDS.length);
+        String blockWord = WORDS[word];
+
+        Scanner scanner = new Scanner(System.in);
+        do{
+            System.out.print("Введи слово: ");
+            String userWord = scanner.nextLine();
+
+            if (isWin(userWord,blockWord)){
+                System.out.println("Ты угадал!");
+                break;
+            }
+            System.out.println("Ты не угадал.Дам тебе подсказку");
+            hintWord(blockWord,userWord);
+            System.out.println();
+
+            }while (true);
+        System.out.println("Game over");
+        }
+
+
+    private static void hintWord(String blockWord, String userWord) {
+        for ( int i= 0;i<BLOCK_WORDS;i++){
+            char hint = '*';
+            if (i < userWord.length() && i<blockWord.length() && userWord.charAt(i)==blockWord.charAt(i) ){
+                hint = userWord.charAt(i);
+            }
+            System.out.println(hint);
+    }
+}
+    private static boolean isWin(String userWord, String blockWord) {
+        return blockWord.equals(userWord);
+
+    }
+
+}
