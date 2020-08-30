211efc1e57d6f75dbacd723067abf09c51d1b1b2
Ivan <ivan.horivchuk@gmail.com>
2020-08-31 01:43:48 +0200

    Homework 3

--- src/homework3/task1.java
+++ src/homework3/task1.java
@@ -5,8 +5,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class task1 {
-    public static final int BLOCK_WORDS= 15;
-    public static final String[] WORDS = new String[] {
+    public static final int BLOCK_WORDS = 15;
+    public static final String[] WORDS = new String[]{
             "apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
             "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
             "pear", "pepper", "pineapple", "pumpkin", "potato"};
@@ -18,35 +18,30 @@ public class task1 {
         String blockWord = WORDS[word];
 
         Scanner scanner = new Scanner(System.in);
-        do{
+        do {
             System.out.print("Введи слово: ");
             String userWord = scanner.nextLine();
 
-            if (isWin(userWord,blockWord)){
+            if (userWord.equals(blockWord)) {
                 System.out.println("Ты угадал!");
                 break;
             }
             System.out.println("Ты не угадал.Дам тебе подсказку");
-            hintWord(blockWord,userWord);
+            hintWord(blockWord, userWord);
             System.out.println();
 
-            }while (true);
+        } while (true);
         System.out.println("Game over");
-        }
+    }
 
 
     private static void hintWord(String blockWord, String userWord) {
-        for ( int i= 0;i<BLOCK_WORDS;i++){
+        for (int i = 0; i < BLOCK_WORDS; i++) {
             char hint = '*';
-            if (i < userWord.length() && i<blockWord.length() && userWord.charAt(i)==blockWord.charAt(i) ){
+            if (i < userWord.length() && i < blockWord.length() && userWord.charAt(i) == blockWord.charAt(i)) {
                 hint = userWord.charAt(i);
             }
             System.out.println(hint);
+        }
     }
-}
-    private static boolean isWin(String userWord, String blockWord) {
-        return blockWord.equals(userWord);
-
-    }
-
 }
